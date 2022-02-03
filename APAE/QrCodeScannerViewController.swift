856177022ae84137230cc96889b6383f436c6cc8
ruicosta1933 <ruicosta607@gmail.com>
2022-02-03 02:18:06 +0000

    QRScanner to Link

---
+++ APAE/QrCodeScannerViewController.swift
@@ -0,0 +1,72 @@
+//
+//  QrCodeScannerViewController.swift
+//  APAE
+//
+//  Created by Rui Costa on 03/02/2022.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import SafariServices
+
+class QrCodeScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
+    static let identifier = "QrCodeScannerViewController"
+    var video = AVCaptureVideoPreviewLayer()
+    @IBOutlet var qrImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Creating session
+        let session = AVCaptureSession()
+        
+        //Define capture device
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do{
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        }
+        catch{
+            print("error")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+   
+        
+        view.layer.addSublayer(video)
+        self.view.bringSubviewToFront(qrImage)
+        session.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != 0 {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
+                if object.type == AVMetadataObject.ObjectType.qr{
+                    let url = URL(string: object.stringValue!)
+                    
+                    
+                    let alert = UIAlertController(title: "QR CODE", message: "tem um link", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Abrir", style: .default, handler: { (nil) in
+                        let vc = SFSafariViewController(url: url!)
+                        self.present(vc, animated: true)
+                    }))
+                    alert.addAction(UIAlertAction(title: "Cancelar", style: .default, handler: nil))
+                    
+                    present(alert, animated: true, completion: nil)
+                    
+                }
+            }
+        }
+    }
+    
+}
